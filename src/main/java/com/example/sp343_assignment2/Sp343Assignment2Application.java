b5c86e23703c73ba6d96d2e894a624e0235bf45e
Supavate <supavate@gmail.com>
2025-02-13 19:10:45 +0700

    remove unused import

--- src/main/java/com/example/sp343_assignment2/Sp343Assignment2Application.java
+++ src/main/java/com/example/sp343_assignment2/Sp343Assignment2Application.java
@@ -1,11 +1,7 @@
 package com.example.sp343_assignment2;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Sp343Assignment2Application {
